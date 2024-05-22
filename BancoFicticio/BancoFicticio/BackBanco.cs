8c841f46de3de74a67146f823665e00c60deff68
João Pedro VL <64650215+wizardbr41@users.noreply.github.com>
2024-05-22 15:44:27 -0300

    Add files via upload

---
+++ BancoFicticio/BancoFicticio/BackBanco.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoFicticio
+{
+    internal class BackBanco
+    {
+        public string Nome { get ; set; }
+        public int NumConta {  get;  private set; }
+        public double Valor { get; private set; }
+
+
+
+
+        //Construtor
+        public BackBanco(string nome, int numconta)
+        {
+            Nome = nome;
+            NumConta = numconta;
+            Valor = 0;
+        }
+
+
+        public void deposito(double valor)
+        {
+            Valor += valor;
+        }
+
+        public void saque(double valor)
+        {
+            Valor = Valor - valor - 5;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {NumConta}, Titular: {Nome}, Saldo: R${Valor}.00"; 
+        }
+
+    }
+}
---
+++ BancoFicticio/BancoFicticio/Program.cs
@@ -0,0 +1,58 @@
+﻿using BancoFicticio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        double valor;
+
+        Console.Write("Entre com sua conta: ");
+        int conta = int.Parse(Console.ReadLine());
+
+        Console.Write("Entre com seu nome: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Havera deposito inicial (S/N)? ");
+        string deposito = Console.ReadLine();
+
+        //construtor
+        BackBanco bb = new BackBanco(nome, conta);
+
+
+
+        //logica de deposito inicial
+        if (deposito == "s")
+        {
+            Console.Write("Entre com o valor de deposito inicial: ");
+            valor = double.Parse(Console.ReadLine());
+            bb.deposito(valor);
+        }
+
+        //mostrar dados da conta
+        Console.WriteLine("Dados da conta:");
+        Console.WriteLine(bb.ToString());
+
+        Console.Write("Entre com valor para deposito: ");
+        valor = double.Parse(Console.ReadLine());
+        bb.deposito(valor);
+
+        Console.WriteLine("Dadis da conta atualizados: ");
+        Console.WriteLine(bb.ToString());
+
+        Console.Write("Entre com um valor para saque: ");
+        valor = double.Parse(Console.ReadLine());
+        bb.saque(valor);
+        Console.WriteLine("Dados da conta atualizados: ");
+        Console.WriteLine(bb.ToString());
+
+
+
+
+
+    }
+}
